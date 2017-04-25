e1f53f8de15f59a230236077370e65d973450623
sena <sena@sena-MS-7817>
2017-04-24 23:01:09 -0500

    Este es mi primer commit sorner

---
+++ src/davidmontoya/Persona1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package davidmontoya;
+
+/**
+ *
+ * @author sena
+ */
+public class Persona1 {
+    private String nombre;
+    public void setNombre (String nombreParametro)
+    {
+        this.nombre=nombreParametro;
+    }
+    public String getNombre()
+    {
+return this.nombre;
+    }
+    public void andar()
+    {
+        System.out.print(nombre+"Está caminando");
+    }
+}
